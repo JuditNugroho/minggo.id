e19eed04c69c836271648bf39c27a82538de6cee
Kevin Hendra William <kevinhendrawiliam@gmail.com>
2019-07-25 15:18:47 +0700

    five

--- resources/views/user/pages/home.blade.php
+++ resources/views/user/pages/home.blade.php
@@ -1,36 +1,21 @@
 @extends('user.layouts.layouthome')
 @section('title', 'Halaman Utama')
 @section('content')
+
+    <div class="col-md-6">
     <h2 align="center">Top Up</h2>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <div class="single-games-area text-center mb-100 wow fadeInUp" data-wow-delay="300ms">
-                    <a href="{{route('topupaov')}}"><img src="/template/img/bg-img/aov.jpg" alt=""></a>
+            <div class="form-style-11">
+                <div class="inner-wrap">
+                    <div><a href="{{route('topupaov')}}"><img src="/template/img/bg-img/aov.jpg" alt=""></a></div>
+
                 </div>
             </div>
-            <div class="col-12 col-md-4">
-                <div class="single-games-area text-center mb-100 wow fadeInUp" data-wow-delay="500ms">
-                    <a href="{{route('topupff')}}"><img src="/template/img/bg-img/FF.jpg" alt=""></a>
-                </div>
+        <div class="form-style-11"><div class="inner-wrap">
+                <div><a href="{{route('topupaov')}}"><img src="/template/img/bg-img/FF.jpg" alt=""></a></div>
             </div>
         </div>
     </div>
-    <h2 align="center">Tier Boster</h2>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-md-6">
-                <div class="single-games-area text-center mb-100 wow fadeInUp" data-wow-delay="300ms">
-                    <a href="{{route('tierbosteraov')}}"><img src="/template/img/bg-img/aov.jpg" alt=""></a>
-                </div>
-            </div>
-            <div class="col-12 col-md-4">
-                <div class="single-games-area text-center mb-100 wow fadeInUp" data-wow-delay="500ms">
-                    <a href="{{route('tierbosteraov')}}"><img src="/template/img/bg-img/FF.jpg" alt=""></a>
-                </div>
-            </div>
-        </div>
-    </div>
+
     <script src="/template/js/jquery/jquery-2.2.4.min.js"></script>
     <script src="/template/js/bootstrap/popper.min.js"></script>
     <script src="/template/js/bootstrap/bootstrap.min.js"></script>
